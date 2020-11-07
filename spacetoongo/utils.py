7303c2d78be25dd2186ee0f7cdc05e6c2a231f45
medram <mramouchy@gmail.com>
2020-11-07 22:21:48 +0100

    Download manager for TS video format

---
+++ spacetoongo/utils.py
@@ -0,0 +1,113 @@
+import os
+import tempfile
+import requests
+from concurrent import futures
+
+
+class DownloadManager:
+    def __init__(self, url, buffer_size=1024):
+        self._url = url
+        self._prefix_url = self._url.split('.smil/')[0] + '.smil/'
+        self._ts_links = self.parse_ts_links()
+        self._junk_files = []
+        self._buffer_size = buffer_size  # in bytes
+
+    def parse_ts_links(self):
+        try:
+            res = requests.get(self._url)
+            lines = res.text.split('\n')
+            self._ts_links = [self._prefix_url +
+                              line for line in lines if not line.startswith('#') and line != '' and '.ts' in line]
+            return self._ts_links
+        except requests.exceptions.RequestException:
+            print('Oops! Connection error')
+
+    def get_ts_links(self):
+        return self._ts_links[:5]
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        print('clean up junk')
+        for junk in self._junk_files:
+            try:
+                os.remove(junk)
+            except OSError:
+                pass
+
+    # dist is a distination file
+    def download_and_save(self, dist, buffer_size=None):
+
+        if not os.path.isdir(os.path.dirname(dist)):
+            raise NotADirectoryError(f"'{dist}' is not a directory")
+        if os.path.isfile(dist):
+            raise FileExistsError(f"'{dist}' is a file, and it's shouldn't be")
+
+        if buffer_size is None:
+            buffer_size = self._buffer_size
+
+        # this temp folder will be remove with its files one we get out from this function.
+        tempdir = tempfile.TemporaryDirectory(prefix='spacetoon.')
+        chunk_paths = self._download_ts_files(tempdir)
+        self._merge_ts_files(dist, chunk_paths)
+        # add files to junk to remove it later in __exit__ function.
+        self._junk_files.extend(chunk_paths)
+
+    def _download_ts_files(self, tempdir, buffer_size=None):
+        print(f'Downloading ({len(self.get_ts_links())}) ts files')
+        """Downloading ts files in parallel"""
+        # with futures.ThreadPoolExecutor() as executor:
+        #     for link, save_to in zip(links, executor.map(self.download, self.get_ts_links())):
+        #         pass
+
+        if buffer_size is None:
+            buffer_size = self._buffer_size
+
+        chunk_paths = {}
+        with futures.ThreadPoolExecutor() as executor:
+            futures_list = {executor.submit(
+                self.download, link, tempfile.mkstemp(dir=tempdir.name)[1], buffer_size): link for link in self.get_ts_links()}
+
+            for future in futures.as_completed(futures_list):
+                try:
+                    link = futures_list[future]
+                    # returns path of downloaded chunk.
+                    chunk_paths.update({link: future.result()})
+                except Exception as e:
+                    print(e)
+            print('[Downloaded Successfully]')
+            # sorting chunks in correct order
+        return [chunk_paths[l] for l in self.get_ts_links()]
+
+    def _merge_ts_files(self, dist, chunk_paths, buffer_size=None):
+        print('Merging ts files')
+        if buffer_size is None:
+            buffer_size = self._buffer_size
+
+        with open(dist, 'wb') as video:
+            for file in chunk_paths:
+                print(f'[Coping] ...', file)
+                with open(file, 'rb') as f:
+                    chunk = f.read(buffer_size)
+                    while chunk:
+                        video.write(chunk)
+                        chunk = f.read(buffer_size)
+
+    @staticmethod
+    def download(url, save_to=None, chunk_size=128):
+        print(f'[Downloding] ...{url}')
+        if save_to is None:
+            save_to = tempfile.mkstemp()[1]
+
+        while True:
+            try:
+                res = requests.get(url, stream=True)
+                with open(save_to, 'wb') as f:
+                    for chunk in res.iter_content(chunk_size=chunk_size):
+                        f.write(chunk)
+                return save_to
+            except requests.exceptions.RequestException:
+                pass
+            except IOError:
+                raise
