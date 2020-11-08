206ac8cfb61fa45bb9b33d3b8696159d7b1382a4
medram <mramouchy@gmail.com>
2020-11-08 18:35:49 +0100

    added: exceptions

---
+++ spacetoongo/exceptions.py
@@ -0,0 +1,14 @@
+class AccountException(Exception):
+    pass
+
+
+class UnsubscribedAccount(AccountException):
+    pass
+
+
+class AccountPermissionError(AccountException):
+    def __init__(self,
+                 msg='This SpacetoonGo account can\'t get access to these assets (try a premium account).'):
+
+        self.message = msg
+        super().__init__(self.message)
